5edb07a5f9e0b7fe800f9cb5fedc4c50c755b5a0
prathakpr <pulkitrathore770@gmail.com>
2024-03-04 20:22:52 +0530

    added some of skeletal mesh cose

--- Source/LearningEngine/Private/Pawn/Bird.cpp
+++ Source/LearningEngine/Private/Pawn/Bird.cpp
@@ -2,13 +2,21 @@
 
 
 #include "Pawn/Bird.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/SkeletalMeshComponent.h"
 
 // Sets default values
 ABird::ABird()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	Capsule = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Apna Capsule"));
+	SetRootComponent(Capsule);
+	Capsule->SetCapsuleHalfHeight(20.f);
+	Capsule->SetCapsuleRadius(15.f);
 
+	BirdMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Bird Mesh"));
+	BirdMesh->SetupAttachment(Capsule);
 }
 
 // Called when the game starts or when spawned
--- Source/LearningEngine/Public/Pawn/Bird.h
+++ Source/LearningEngine/Public/Pawn/Bird.h
@@ -12,18 +12,20 @@ class LEARNINGENGINE_API ABird : public APawn
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this pawn's properties
+	
 	ABird();
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+protected:
+	
+	virtual void BeginPlay() override;
+	UPROPERTY(VisibleAnywhere)
+	class UCapsuleComponent* Capsule; //forward declaration
+	UPROPERTY(VisibleAnywhere)
+	class USkeletalMeshComponent* BirdMesh;
+	
+	
 };
